faa9b72636c4a5c558e9bc86a9dc974a78ec0b8e
SubhodipShee <subhodipshee17@gmail.com>
2025-07-30 20:41:08 +0530

    minor bugs fixes

--- frontend/src/components/AdminProductCard.js
+++ frontend/src/components/AdminProductCard.js
@@ -1,4 +1,4 @@
-import React,  { useState } from 'react'
+/* import React,  { useState } from 'react'
 import { MdModeEditOutline } from "react-icons/md";
 import AdminEditProduct from './AdminEditProduct';
 import displayINRCurrency from '../helpers/displayCurrency';
@@ -45,4 +45,56 @@ return (
 )
 }
 
-export default AdminProductCard
+export default AdminProductCard */
+
+import React, { useState } from 'react';
+import { MdModeEditOutline } from "react-icons/md";
+import AdminEditProduct from './AdminEditProduct';
+import displayINRCurrency from '../helpers/displayCurrency';
+
+const AdminProductCard = ({ data, fetchdata }) => {
+  const [editProduct, setEditProduct] = useState(false);
+
+  // Make sure `productImage` is an array and has at least one image
+  const productImage = data?.productImage && data.productImage.length > 0 ? data.productImage[0] : 'default-image-url.jpg';
+
+  return (
+    <div className='bg-white p-4 rounded'>
+      <div className='w-40'>
+        <div className='w-32 h-32 flex justify-center items-center'>
+          {/* Safely render the first product image */}
+          <img 
+            src={productImage} 
+            alt={data?.productName || 'Product Image'} 
+            className='mx-auto object-fill h-full' 
+          />
+        </div> 
+        <h1 className='text-ellipsis line-clamp-2'>{data?.productName || 'Product Name'}</h1>
+
+        <div>
+          <p className='font-semibold'>
+            {data?.sellingPrice ? displayINRCurrency(data.sellingPrice) : 'Price Not Available'}
+          </p>
+
+          <div 
+            className='w-fit ml-auto p-2 bg-green-100 hover:bg-green-600 rounded-full hover:text-white cursor-pointer'
+            onClick={() => setEditProduct(true)}
+          >
+            <MdModeEditOutline />
+          </div>
+        </div>
+      </div>
+
+      {editProduct && (
+        <AdminEditProduct 
+          productData={data} 
+          onClose={() => setEditProduct(false)} 
+          fetchdata={fetchdata}
+        />
+      )}
+    </div>
+  );
+};
+
+export default AdminProductCard;
+
--- frontend/src/pages/AllUsers.js
+++ frontend/src/pages/AllUsers.js
@@ -62,7 +62,7 @@ const AllUsers = () => {
           {
             allUser.map((el,index) => {
                 return(
-                  <tr>
+                  <tr key={el._id || index}>
                     <td>{index+1}</td>
                     <td>{el?.name}</td>
                     <td>{el?.email}</td>
